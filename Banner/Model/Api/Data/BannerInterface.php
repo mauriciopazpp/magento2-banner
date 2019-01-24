fe30945df31ef63f64de909f281c32397c3fc439
Mauricio Paz <mauriciopazdev@gmail.com>
2019-01-23 23:56:46 -0200

    Banner using Interface

---
+++ Banner/Model/Api/Data/BannerInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Mauricio\Banner\Model\Api\Data;
+
+interface BannerInterface
+{
+	const ID = 'id';
+	const CONTENT = 'content';
+	const LINK = 'link';
+	const ENABLE = 'enable';
+	const CREATED_AT = 'created_at';
+	const UPDATED_AT = 'updated_at';
+
+	public function getId();
+	public function setId($id);
+	
+	public function getContent();
+	public function setContent($content);
+
+	public function getLink();
+	public function setLink($link);
+
+	public function getEnable();
+	public function setEnable($enable);
+	
+	public function getCreatedAt();
+	public function setCreatedAt($createdAt);
+
+	public function getUpdatedAt();
+	public function setUpdatedAt($updatedAt);
+}
--- Banner/Model/Banner.php
+++ Banner/Model/Banner.php
@@ -2,7 +2,11 @@
 
 namespace Mauricio\Banner\Model;
 
-class Banner extends \Magento\Framework\Model\AbstractModel implements \Magento\Framework\DataObject\IdentityInterface
+use \Magento\Framework\Model\AbstractModel;
+use Mauricio\Banner\Model\Api\Data\BannerInterface;
+use Magento\Framework\DataObject\IdentityInterface;
+
+class Banner extends AbstractModel implements IdentityInterface, BannerInterface
 {
 	const CACHE_TAG = 'mauricio_banner_banner';
 	protected $_cacheTag = 'mauricio_banner_banner';
@@ -11,6 +15,66 @@ class Banner extends \Magento\Framework\Model\AbstractModel implements \Magento\
 	protected function _construct()
 	{
 		$this->_init('Mauricio\Banner\Model\ResourceModel\Banner');
+	}
+
+	public function getBannerId()
+    {
+        return $this->getData(self::ID);
+    }
+
+    public function setBannerId($id)
+    {
+        return $this->setData(self::ID, $id);
+    }
+
+    public function getContent()
+    {
+        return $this->getData(self::CONTENT);
+    }
+
+    public function setContent($content)
+    {
+        return $this->setData(self::CONTENT, $content);
+    }
+
+    public function getLink()
+    {
+        return $this->getData(self::LINK);
+    }
+
+	public function setLink($link)
+	{
+		return $this->setData(self::LINK, $link);
+	}
+
+    public function getEnable()
+    {
+        return $this->getData(self::ENABLE);
+    }
+
+	public function setEnable($enable)
+	{
+		return $this->setData(self::ENABLE, $enable);
+	}
+
+    public function getCreatedAt()
+    {
+        return $this->getData(self::CREATED_AT);
+    }
+
+	public function setCreatedAt($createdAt)
+	{
+		return $this->setData(self::CREATED_AT, $createdAt);
+	}
+	
+    public function getUpdatedAt()
+    {
+        return $this->getData(self::CREATED_AT);
+    }
+
+	public function setUpdatedAt($updatedAt)
+	{
+		return $this->setData(self::UPDATED_AT, $updatedAt);
 	}
 
 	public function getIdentities()
